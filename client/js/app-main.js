9e64dcf35d92bb5ab3d12ff4fe6cf40f7868a882
cbeausei <clementbeauseigneur@gmail.com>
2021-04-05 19:48:06 +0200

    Style landing page

--- client/js/app-main.js
+++ client/js/app-main.js
@@ -19,6 +19,109 @@ class AppMain extends LitElement {
     this.players = null;
   }
 
+  // Shared styles.
+
+  getSharedStyles() {
+    return html` 
+      <style>
+        :host {
+          --color1: rgba(255, 255, 255, 0.87);
+          --color2: #1C4AF8;
+          --color3: black;
+          background-color: var(--color3);
+          color: var(--color1);
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+        }
+        [xlarge] {
+          font-size: 24px;
+        }
+        [large] {
+          font-size: 20px;
+        }
+        [medium] {
+          font-size: 16px;
+        }
+        [small] {
+          font-size: 14px;
+        }
+        [xsmall] {
+          font-size: 12px;
+        }
+        [button] {
+          background-color: var(--color2);
+          border-radius: 10px;
+          box-shadow: inset 0 0 4px #000000;
+          padding: 5px 15px;
+        }
+        [button]:hover {
+          box-shadow: inset 0 0 12px #000000;
+          cursor: pointer;
+        }
+      </style>
+    `
+  }
+
+  // Templates below.
+
+  renderLandingPage() {
+    return html`
+      ${this.getSharedStyles()}
+      <style>
+        :host {
+          align-items: center;
+          justify-content: center;
+        }
+      </style>
+      <div button xlarge @click="${this.createGame}">Create a new game</div>
+    `;
+  }
+
+  renderNickSelectionPage() {
+    return html`
+      <h2>Pick a nickname:</h2>
+      <input id="input" type="text">
+      <button @click="${this.createPlayer}">Enter</button>
+    `;
+  }
+
+  renderPreGamePage() {
+    return html`
+      <h1>Waiting lobby.</h1>
+      <p>Share the following link to your friends to let them join the game:</p>
+      <p>${location.origin}/?gameId=${this.gameId}</p>
+      <h2>Players connected</h2>
+      <ul>
+        ${this.players.map(nick => html`
+          <li>
+            ${nick === this.nick ? html`<b>${nick}</b>` : html`${nick}`}
+          </li>
+        `)}
+      </ul>
+      ${this.players.length > 1 ? html`<button @click="${this.startGame}">Start game</button>` : html``}
+    `;
+  }
+// ${this.players.length > 1 ? html`<button @click="${this.startGame}">Start game</button>`}
+  renderGamePage() {
+    return html`
+      <p>Game is starting! There are ${this.players.length} players registered.</p>
+    `
+  }
+
+  render() {
+    if (this.gameId == null) {
+      return this.renderLandingPage();
+    }
+    if (this.playerId == null) {
+      return this.renderNickSelectionPage();
+    }
+    if (!this.gameStarted) {
+      return this.renderPreGamePage();
+    }
+    return this.renderGamePage();
+  }
+
   startGame() {
     this.gameStarted = true;
   }
@@ -62,57 +165,7 @@ class AppMain extends LitElement {
     if (update.players) {
       this.players = update.players;
     }
-  }
-  
-  render() {
-    if (this.gameId == null) {
-      return this.renderLandingPage();
-    }
-    if (this.playerId == null) {
-      return this.renderNickSelectionPage();
-    }
-    if (!this.gameStarted) {
-      return this.renderPreGamePage();
-    }
-    return this.renderGamePage();
-  }
-
-  renderLandingPage() {
-    return html`
-      <button @click="${this.createGame}">Create a new game</button>
-    `;
-  }
-
-  renderNickSelectionPage() {
-    return html`
-      <h2>Pick a nickname:</h2>
-      <input id="input" type="text">
-      <button @click="${this.createPlayer}">Enter</button>
-    `;
-  }
-
-  renderPreGamePage() {
-    return html`
-      <h1>Waiting lobby.</h1>
-      <p>Share the following link to your friends to let them join the game:</p>
-      <p>${location.origin}/?gameId=${this.gameId}</p>
-      <h2>Players connected</h2>
-      <ul>
-        ${this.players.map(nick => html`
-          <li>
-            ${nick === this.nick ? html`<b>${nick}</b>` : html`${nick}`}
-          </li>
-        `)}
-      </ul>
-      ${this.players.length > 1 ? html`<button @click="${this.startGame}">Start game</button>` : html``}
-    `;
-  }
-// ${this.players.length > 1 ? html`<button @click="${this.startGame}">Start game</button>`}
-  renderGamePage() {
-    return html`
-      <p>Game is starting! There are ${this.players.length} players registered.</p>
-    `
-  }
+  } 
 }
 
 customElements.define('app-main', AppMain);
